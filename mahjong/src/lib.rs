b2b6d42a77b752e1c8954dacd54e6788b8acdd10
David LeGare <dlegare.1001@gmail.com>
2020-01-14 13:39:36 -0600

    Add function for generating a tileset

--- mahjong/src/lib.rs
+++ mahjong/src/lib.rs
@@ -1,11 +1,14 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+use derive_more::*;
+use strum::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, From)]
 pub enum Tile {
     Simple(SimpleTile),
     Bonus(BonusTile),
     Honor(HonorTile),
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
 pub enum Suit {
     Coins,
     Bamboo,
@@ -18,13 +21,13 @@ pub struct SimpleTile {
     pub suit: Suit,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, From)]
 pub enum BonusTile {
     Flower(Flower),
     Season(Season),
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
 pub enum Flower {
     PlumBlossom,
     Orchid,
@@ -32,7 +35,7 @@ pub enum Flower {
     Bamboo,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
 pub enum Season {
     Spring,
     Summer,
@@ -40,13 +43,13 @@ pub enum Season {
     Winter,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, From)]
 pub enum HonorTile {
     Wind(Wind),
     Dragon(Dragon),
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
 pub enum Wind {
     East,
     South,
@@ -54,9 +57,57 @@ pub enum Wind {
     North,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
 pub enum Dragon {
     Red,
     Green,
     White,
+}
+
+pub fn generate_tileset() -> Vec<Tile> {
+    let mut tiles = Vec::with_capacity(144);
+
+    // Add simple tiles for each suit:
+    //
+    // * Tiles in each suit are numbered 1-9.
+    // * There are four copies of each simple tile.
+    for suit in Suit::iter() {
+        for number in 1..=9 {
+            for _ in 0..4 {
+                tiles.push(SimpleTile { suit, number }.into());
+            }
+        }
+    }
+
+    // Add honor tiles:
+    //
+    // * There are dragon and wind honors.
+    // * There are four copies of each honor tile.
+
+    for dragon in Dragon::iter() {
+        for _ in 0..4 {
+            tiles.push(HonorTile::Dragon(dragon).into());
+        }
+    }
+
+    for wind in Wind::iter() {
+        for _ in 0..4 {
+            tiles.push(HonorTile::Wind(wind).into());
+        }
+    }
+
+    // Add the bonus tiles:
+    //
+    // * There are flower and wind bonus tiles.
+    // * There is only one of each bonus tile.
+
+    for flower in Flower::iter() {
+        tiles.push(BonusTile::Flower(flower).into());
+    }
+
+    for season in Season::iter() {
+        tiles.push(BonusTile::Season(season).into());
+    }
+
+    tiles
 }
