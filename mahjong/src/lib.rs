2ecdcbbe094a76db4cf9615569b8e4f5a3873298
David LeGare <dlegare.1001@gmail.com>
2020-01-17 17:05:11 -0600

    Properly actually pass a string to C#

--- mahjong/src/lib.rs
+++ mahjong/src/lib.rs
@@ -1,5 +1,7 @@
 use derive_more::*;
+use std::{ffi::CString, os::raw::c_char};
 use strum::*;
+use wasm_bindgen::prelude::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, From)]
 pub enum Tile {
@@ -110,4 +112,24 @@ pub fn generate_tileset() -> Vec<Tile> {
     }
 
     tiles
+}
+
+#[wasm_bindgen]
+pub fn generate_tileset_json() -> String {
+    // TODO: Actually generate the tileset and serialize it to JSON.
+    "[]".into()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn __cs_bindgen_generate_tileset_json(out_len: *mut i64) -> *mut c_char {
+    let json = generate_tileset_json();
+    *out_len = json.len() as i64;
+
+    let result = CString::new(json).expect("Generated string contained a null byte");
+    result.into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn __cs_bindgen_drop_string(raw: *mut c_char) {
+    let _ = CString::from_raw(raw);
 }
