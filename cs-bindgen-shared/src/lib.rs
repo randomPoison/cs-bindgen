35f934dbe0c68745443819b07966c919db58ed0d
David LeGare <dlegare.1001@gmail.com>
2020-02-14 17:49:37 -0600

    Merge metadata types in cs-bindgen-shared with schematic types

--- cs-bindgen-shared/src/lib.rs
+++ cs-bindgen-shared/src/lib.rs
@@ -1,22 +1,30 @@
+use derive_more::From;
 use schematic::Schema;
+use schematic::Struct;
 use serde::*;
-
-pub mod meta;
 
 // Re-export schematic so that dependent crates don't need to directly depend on it.
 pub use schematic;
 
+/// An item exported from the Rust as a language binding.
+#[derive(Debug, Clone, From, Serialize, Deserialize)]
+pub enum Export {
+    Fn(Func),
+    Struct(Struct),
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct Describe {
+pub struct Func {
     pub name: String,
+    pub binding: String,
     pub receiver: Option<Receiver>,
-    pub inputs: Vec<Schema>,
+    pub inputs: Vec<(Option<String>, Schema)>,
     pub output: Schema,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Receiver {
-    pub name: String,
+    pub ty: Schema,
     pub style: ReceiverStyle,
 }
 
--- cs-bindgen-shared/src/meta/strct.rs
+++
@@ -1,6 +0,0 @@
-use serde::*;
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Struct {
-    pub ident: String,
-}
