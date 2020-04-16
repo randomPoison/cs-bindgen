a44195baf1fc02fef90ac66d00e045f6a1dc289e
David LeGare <dlegare.1001@gmail.com>
2020-04-16 14:59:46 -0500

    Add tests for returning lists of primitives

--- integration-tests/TestRunner/Collections.cs
+++ integration-tests/TestRunner/Collections.cs
@@ -8,7 +8,7 @@ namespace TestRunner
         [Fact]
         public void ReturnVecInt()
         {
-            var expected = IntegrationTests.ReturnVec();
+            var expected = IntegrationTests.ReturnVecI32();
             var actual = new List<int>() { 1, 2, 3, 4 };
             Assert.Equal(expected, actual);
         }
@@ -18,10 +18,26 @@ namespace TestRunner
         {
             for (var count = 0; count < 100_000; count += 1)
             {
-                var expected = IntegrationTests.ReturnVec();
+                var expected = IntegrationTests.ReturnVecI32();
                 var actual = new List<int>() { 1, 2, 3, 4 };
                 Assert.Equal(expected, actual);
             }
+        }
+
+        [Fact]
+        public void ReturnVecFloat()
+        {
+            var expected = IntegrationTests.ReturnVecF32();
+            var actual = new List<float>() { 1.0f, 2.1f, 3.123f, 4.00000004f };
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnVecBool()
+        {
+            var expected = IntegrationTests.ReturnVecBool();
+            var actual = new List<bool>() { true, false, true, true };
+            Assert.Equal(expected, actual);
         }
     }
 }
--- integration-tests/src/collections.rs
+++ integration-tests/src/collections.rs
@@ -3,6 +3,56 @@
 use cs_bindgen::prelude::*;
 
 #[cs_bindgen]
-pub fn return_vec() -> Vec<i32> {
+pub fn return_vec_i8() -> Vec<i8> {
     vec![1, 2, 3, 4]
+}
+
+#[cs_bindgen]
+pub fn return_vec_u8() -> Vec<u8> {
+    vec![1, 2, 3, 4]
+}
+
+#[cs_bindgen]
+pub fn return_vec_i16() -> Vec<i16> {
+    vec![1, 2, 3, 4]
+}
+
+#[cs_bindgen]
+pub fn return_vec_u16() -> Vec<u16> {
+    vec![1, 2, 3, 4]
+}
+
+#[cs_bindgen]
+pub fn return_vec_i32() -> Vec<i32> {
+    vec![1, 2, 3, 4]
+}
+
+#[cs_bindgen]
+pub fn return_vec_u32() -> Vec<u32> {
+    vec![1, 2, 3, 4]
+}
+
+#[cs_bindgen]
+pub fn return_vec_i64() -> Vec<i64> {
+    vec![1, 2, 3, 4]
+}
+
+#[cs_bindgen]
+pub fn return_vec_u64() -> Vec<u64> {
+    vec![1, 2, 3, 4]
+}
+
+#[cs_bindgen]
+pub fn return_vec_f32() -> Vec<f32> {
+    vec![1.0, 2.1, 3.123, 4.00000004]
+}
+
+#[cs_bindgen]
+pub fn return_vec_f64() -> Vec<f64> {
+    vec![1.0, 2.1, 3.123, 4.00000004]
+}
+
+#[cs_bindgen]
+pub fn return_vec_bool() -> Vec<bool> {
+    vec![true, false, true, true]
 }
